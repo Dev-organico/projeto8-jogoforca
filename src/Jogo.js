b93fe4b6c6e6ddc3da573284f797e34b639e7e0d
Dev-organico <joao.zolet@gmail.com>
2022-11-27 00:11:14 -0300

    teste 3

--- src/Jogo.js
+++ src/Jogo.js
@@ -16,7 +16,7 @@ export default function Jogo({count,randWord,showWord,changeColor,arrayWord,rand
                 </div>
                 <div className="right_content">
                     <button data-test="choose-word" onClick={showWord}  className="bt" type="button">Escolher Palavra</button>
-                    <p  data-test="word" data-answer={rand} className={`randWord ${changeColor}`} >{count === 6?arrayWord.join(""):randWord}</p>
+                    <p  data-test="word" data-answer={arrayWord.join("")} className={`randWord ${changeColor}`} >{count === 6?arrayWord.join(""):randWord}</p>
                 </div>
             </div>
 
